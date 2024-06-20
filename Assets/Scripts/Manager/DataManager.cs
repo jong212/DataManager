bcb823d5d2320bbe54ead490314fc517c2ace0ae
조현승 <danitech171791@gmail.com>
2024-06-20 14:50:51 +0900

    [feat] DataManaging - 데이터 매니저 로드된 데이터 보관부
    - 게임 시작 시 1번만 로드해도 되는 데이터들을 미리 읽어두고, 딕셔너리에 보관해두자

---
+++ Assets/Scripts/Manager/DataManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    Dictionary<int, Character> _loadedCharacterList = new Dictionary<int, Character>();
+    Dictionary<string, Buff> _loadedBuffList = new Dictionary<string, Buff>();
+    Dictionary<string, Skill> _loadedSkillList = new Dictionary<string, Skill>();
+}
