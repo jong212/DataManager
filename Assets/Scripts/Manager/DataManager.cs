22bede66dc7ffd4b6498f8ed2f51928027c3e2fa
조현승 <danitech171791@gmail.com>
2024-06-20 14:52:00 +0900

    [feat] DataManaging - 데이터 로드 Root 경로를 설정할 수 있게 전역변수 선언
    - 추후 Application.path 같은걸로 앱 내 데이터를 참조할 수 있게 해주자

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -8,4 +8,6 @@ public class DataManager : MonoBehaviour
     Dictionary<int, Character> _loadedCharacterList = new Dictionary<int, Character>();
     Dictionary<string, Buff> _loadedBuffList = new Dictionary<string, Buff>();
     Dictionary<string, Skill> _loadedSkillList = new Dictionary<string, Skill>();
+
+    private readonly string _dataRootPath = "C:/Users/KGA/Desktop/DataParser";
 }
