73427b01864ad9bfd6d6704d492796b3def8cbbf
조현승 <danitech171791@gmail.com>
2024-06-20 15:11:21 +0900

    [feat] DataManaging - Extension / 버프랑 스킬 데이터도 가져오기

--- Assets/Scripts/Manager/DataManigingExtensions.cs
+++ Assets/Scripts/Manager/DataManigingExtensions.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public static class DataManigingExtensions
 {
-    public static Character GetCharacter(this DataManager manager, int dataId)
+    public static Character GetCharacterData(this DataManager manager, int dataId)
     {
-        if(manager.LoadedCharacterList.Count == 0
-            || manager.LoadedCharacterList.ContainsKey(dataId) == false)
+        var loadedCharacterList = manager.LoadedCharacterList;
+        if(loadedCharacterList.Count == 0
+            || loadedCharacterList.ContainsKey(dataId) == false)
         {
             return null;
         }
 
-        return manager.LoadedCharacterList[dataId];
+        return loadedCharacterList[dataId];
     }
-   
+
+    public static Skill GetSkillData(this DataManager manager, string dataClassName)
+    {
+        var loadedSkillList = manager.LoadedSkillList;
+        if (loadedSkillList.Count == 0
+            || loadedSkillList.ContainsKey(dataClassName) == false)
+        {
+            return null;
+        }
+
+        return loadedSkillList[dataClassName];
+    }
+
+    public static Buff GetBuffData(this DataManager manager, string dataClassName)
+    {
+        var loadedBuffList = manager.LoadedBuffList;
+        if (loadedBuffList.Count == 0
+           || loadedBuffList.ContainsKey(dataClassName) == false)
+        {
+            return null;
+        }
+
+        return loadedBuffList[dataClassName];
+    }
 }
