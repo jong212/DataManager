f1a67fac77480a9fd199380c1d89091d6e8c5cdd
조현승 <danitech171791@gmail.com>
2024-06-20 14:53:45 +0900

    [feat] DataManaging - 데이터매니저 싱글턴

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
+    public static DataManager Inst { get; private set; }
     Dictionary<int, Character> _loadedCharacterList = new Dictionary<int, Character>();
     Dictionary<string, Buff> _loadedBuffList = new Dictionary<string, Buff>();
     Dictionary<string, Skill> _loadedSkillList = new Dictionary<string, Skill>();
 
     private readonly string _dataRootPath = "C:/Users/KGA/Desktop/DataParser";
+
+    private void Awake()
+    {
+        Inst = this;
+        ReadAllDataOnAwake();
+    }
 }
