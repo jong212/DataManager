452e0e74652ac879594579f61b1bc2fad0521b37
조현승 <danitech171791@gmail.com>
2024-06-20 14:49:02 +0900

    [feat] DataManaging - 모델 추가
    - 데이터로 읽어온걸 class로 매핑시켜줄 수 있게 모델을 추가한다.
    - 이를 자동으로 할 수 있게 개선한 곳도 있으나, Raw한 상태로 구현해보자

---
+++ Assets/Scripts/Model/DataMappers.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public int DataId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string IconPath { get; set; }
+    public string PrefabPath { get; set; }
+
+    public List<string> SkillClassNameList { get; set; }    
+}
+
+public class Skill
+{
+    public string SkillClassName { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int BaseDamage { get; set; }
+    public float DamageMultiSkillLevelName { get; set; }
+    public List<string> BuffNameList { get; set; }
+}
+
+public class Buff
+{
+    public string BuffClassName { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int BuffTime { get; set; }
+    public List<float> BuffValues { get; set; }
+}
