901a3a26e2a48c98833901916c2fc1147373f3d0
조현승 <danitech171791@gmail.com>
2024-07-01 09:53:12 +0900

    [feat] AnimationEvent - 애니메이터에서 특정 clip을 찾아주는 함수 추가

--- Assets/Scripts/AnimationEvent/CharacterAnimationEventFitter.cs
+++ Assets/Scripts/AnimationEvent/CharacterAnimationEventFitter.cs
@@ -7,4 +7,19 @@ public class CharacterAnimationEventFitter : MonoBehaviour
     [SerializeField] Animator Animator_Origin;
 
     private string _temporalClipName = "Dizzy";
+
+    private AnimationClip LoadClipByName(string name)
+    {
+        AnimationClip clip = null;
+        foreach(var animClip in Animator_Origin.runtimeAnimatorController.animationClips)
+        {
+            if(animClip.name == name)
+            {
+                clip = animClip;
+                break;
+            }
+        }
+
+        return clip;
+    }
 }
