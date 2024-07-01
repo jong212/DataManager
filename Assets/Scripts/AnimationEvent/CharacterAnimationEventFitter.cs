8e4c39fd79adf443323d3447b81b134fc980927e
조현승 <danitech171791@gmail.com>
2024-07-01 09:52:05 +0900

    [feat] AnimationEvent - 애니메이션 이벤트 피터 컴포넌트 기초 구조
    - 애니메이터를 등록해 해당 애니메이터에 여러 이벤트들을 연결해주는 역할의 컴포넌트

---
+++ Assets/Scripts/AnimationEvent/CharacterAnimationEventFitter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEventFitter : MonoBehaviour
+{
+    [SerializeField] Animator Animator_Origin;
+
+    private string _temporalClipName = "Dizzy";
+}
