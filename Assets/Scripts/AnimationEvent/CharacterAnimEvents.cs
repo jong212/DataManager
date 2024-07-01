140597917ee07e09bd089e60b8999f11d849dbbb
조현승 <danitech171791@gmail.com>
2024-07-01 11:12:42 +0900

    [feat] AnimationEvent - 이벤트 발생 시 내 gameObject의 특정 컴포넌트를 가져와서 별도 처리 하기
    - 예시는 미리 등록된 이펙트 프리팹을 가져오고, 동적생성한 후에 재생시켜준다.

--- Assets/Scripts/AnimationEvent/CharacterAnimEvents.cs
+++ Assets/Scripts/AnimationEvent/CharacterAnimEvents.cs
@@ -8,4 +8,22 @@ public class CharacterAnimEvents : MonoBehaviour
     {
         Debug.Log("애니 발생 됬다");
     }
+
+    public void OnAnimationEvent_Dizzy()
+    {
+        var tempAnimPrefab = this.GetComponent<TempAnimationPrefab>();
+        if(tempAnimPrefab != null)
+        {
+            var prefab = tempAnimPrefab.GetEffect("dizzy");
+            if(prefab != null)
+            {
+                var effectObj = Instantiate(prefab, this.gameObject.transform);
+                var effect = effectObj.GetComponent<ParticleSystem>();
+                if (effect != null)
+                {
+                    effect.Play();
+                }
+            }
+        }
+    }
 }
