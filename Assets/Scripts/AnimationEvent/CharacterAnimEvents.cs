6a45c449713f1c1193e5d103ab005e47621cf2a6
조현승 <danitech171791@gmail.com>
2024-07-01 09:21:43 +0900

    [feat] AnimationEvent - 특정 클립에서 애니메이션 재생 시 이벤트를 받을 컴포넌트 추가

---
+++ Assets/Scripts/AnimationEvent/CharacterAnimEvents.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimEvents : MonoBehaviour
+{
+    public void OnAnimationEventTest()
+    {
+        Debug.Log("애니 발생 됬다");
+    }
+}
